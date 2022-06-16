60de099fce7873bcbd08783380de0f2ba3c3f20d
tmax818 <tylermaxwell661@gmail.com>
2022-06-16 12:44:39 -0700

    update db order

--- db/template.sql
+++ db/template.sql
@@ -5,6 +5,9 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema template
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `template` ;
@@ -12,7 +15,7 @@ DROP SCHEMA IF EXISTS `template` ;
 -- -----------------------------------------------------
 -- Schema template
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `template` ;
+CREATE SCHEMA IF NOT EXISTS `template` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `template` ;
 
 -- -----------------------------------------------------
@@ -20,14 +23,16 @@ USE `template` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `template`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `first_name` VARCHAR(45) NULL,
-  `last_name` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(255) NULL,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
   `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -35,21 +40,21 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `template`.`things` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `column1` VARCHAR(45) NULL,
-  `column2` VARCHAR(45) NULL,
-  `column3` VARCHAR(45) NULL,
-  `column4` VARCHAR(45) NULL,
+  `column1` VARCHAR(45) NULL DEFAULT NULL,
+  `column2` VARCHAR(45) NULL DEFAULT NULL,
+  `column3` VARCHAR(45) NULL DEFAULT NULL,
+  `column4` VARCHAR(45) NULL DEFAULT NULL,
+  `user_id` INT NOT NULL,
   `created_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `user_id` INT NOT NULL,
   PRIMARY KEY (`id`),
   INDEX `fk_things_users_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_things_users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `template`.`users` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    REFERENCES `template`.`users` (`id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
